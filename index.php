4d6936d7506fd4df453724513f1da1aefeb2569c
nickCovington <nickacovington@gmail.com>
2021-11-13 12:16:45 -0500

    added fixed-bottom to footer

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
 
 
         <!-- FOOTER -->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav class="navbar fixed-bottom navbar-expand-lg navbar-dark bg-dark">
             <a class="navbar-brand" href="#">Footer w/ text</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
