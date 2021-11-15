07c5136470cea101a646771d3f1150b222bdaad6
nickCovington <nickacovington@gmail.com>
2021-11-15 15:17:17 -0500

    veg pic margin 0

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
 
         <!-- NAVBAR powered by bootstrap library -->
         <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">Grocery Share</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -81,7 +81,7 @@
 
         <!-- FOOTER -->
         <nav class="navbar fixed-bottom navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">Footer w/ text</a>
+            <a class="navbar-brand" href="#">Grocery Share</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
